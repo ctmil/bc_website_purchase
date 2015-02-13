f974cac4e5a8736fcf4305ecd681a7fc2cb25303
Torvald B. Bringsvor <torvald@bringsvor.com>
2015-02-13 02:25:56 +0100

    Not making that module after all.

--- models/order.py
+++ models/order.py
@@ -4,3 +4,35 @@ class purchase_order_line(models.Model):
     _inherit = "purchase.order.line"
 
     leadtime = fields.Integer('Leadtime', default=10, help='Requested leadtime in days.')
+
+
+class purchase_requisition(models.Model):
+    _inherit = 'purchase.requisition'
+
+    @api.one
+    def action_purchase_requisition_suppliers(self):
+        vals = {}
+        sellers = [{'supplier': 1290, 'leadtime': 12}, {'supplier': 579, 'leadtime': 27}]
+        vals['product_suppliers'] = [(0, 0, v) for v in sellers]
+
+        wizard = self.env['requisition_suppliers'].create(vals=vals)
+        print "WIZARD CREATED", wizard
+        return wizard
+
+        ir_ui_view_osv = self.env['ir.ui.view']
+        view_id = ir_ui_view_osv.search(
+            [('name', '=', 'view_purchase_requisition_suppliers')]
+        )
+
+
+        return {
+            'name' : 'Select supplier wizard',
+            'view_type' : 'form',
+            'view_mode' : 'form',
+            'res_model' : 'requisition_suppliers',
+            'res_id' : wizard.id,
+            'view_id' : view_id,
+            'type' : 'ir.actions.act_window',
+            'target' : 'new',
+            'context' : self.env.context
+        }
--- wizards/request_relevant_suppliers.py
+++ wizards/request_relevant_suppliers.py
@@ -1,17 +1,165 @@
 __author__ = 'tbri'
 from openerp import fields, models, api, _
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class request_relevant_suppliers(models.TransientModel):
-    _name = "bc_website_purchase.requisition.suppliers"
+    _name = "requisition_suppliers"
     _description = "Purchase Requisition Suppliers"
 
-    product_suppliers = fields.Many2many('res.partner',
-        string='Suppliers', domain=[('supplier', '=', True)])
+    def _get_active_id(self):
+        print "GETTING ACTIVE_ID", self.env.context
+        return self.env.context.get('active_id', False)
 
-    @api.model
-    def view_init(self, fields_list):
-        print "VIEW INIT V8"
+    name = fields.Char('What?')
+    tender = fields.Many2one('purchase.requisition', 'CallForBitds',  default=lambda self: self._get_active_id())
+    numsupp = fields.Integer('Number of suppliers', compute='_get_numsupp')
+    product_suppliers = fields.One2many('relevant_supplierinfo', 'relevant_suppliers',
+        string='Suppliers')
+
+    """
+    @api.depends('tender')
+    def onchange_tender(self):
+        print "ONCHANGE_TENDER"
+        if self.tender:
+            sellers = [{'supplier': 1290, 'leadtime': 12}, {'supplier': 579, 'leadtime': 27}]
+            self.product_suppliers = [(0, 0, v) for v in sellers]
+            _logger.info('product_suppliers onchange %s', self.product_suppliers)
+    """
+
+    @api.one
+    def _get_numsupp(self):
+        self.numsupp = len(self.product_suppliers)
+
+    @api.one
+    def _comp_product_suppliers(self):
+        sellers = [{'supplier': 1290, 'leadtime': 12}, {'supplier': 579, 'leadtime': 27}]
+        self.product_suppliers = [(0, 0, v) for v in sellers]
+        return sellers
+
+
+    @api.v7
+    def Xdefault_get(self, cr, uid, fields_list=None, context=None):
+        dg = self.xxdefault_get(cr, uid)
+        print "DEFAULT GET V7", dg
+        return dg[0]
+
+    @api.one
+    def xxdefault_get(self):
+        vals = {}
+        _logger.info(' CONTEXT %s', self.env.context)
+        active_id = self.env.context['active_id']
+        tender = self.env['purchase.requisition'].browse(active_id)
+        products = [line.product_id for line in tender.line_ids]
+
+        sellers = []
+        for product in products:
+            for seller in product.seller_ids:
+                _logger.info('CREATING SELLER %s',seller.name.name)
+
+                #supp = self.env['bc_website_purchase.relevant.supplierinfo'].create(
+                #    {'supplier' : seller.name.id,
+                #                                 'leadtime' : seller.delay,
+                #                                 'relevant_suppliers' : self.id
+                #    }
+                #)
+                info = {'supplier' : seller.name.id,
+                        'leadtime' : seller.delay,
+                        #'relevant_suppliers' : self.id
+                         }
+                _logger.info('About to create %s', info)
+                #self.product_suppliers = [(0,0, info)]
+                sellers.append(info)
+                _logger.info('SUPP %s', sellers)
+
+        #self.product_suppliers = [(0, 0, v) for v in sellers]
+
+        vals['product_suppliers'] = [(0, 0, v) for v in sellers]
+        return vals
+
+
+    def default_get(self, cr, uid, fields_list=None, context=None):
+        val = {}
+        active_id = context['active_id']
+        tender = self.pool('purchase.requisition').browse(cr, uid, [active_id])
+        products = [line.product_id for line in tender.line_ids]
+        sellers = []
+        for product in products:
+            for seller in product.seller_ids:
+                info = {'name' : seller.name.id,
+                        'leadtime' : seller.delay,
+                        #'relevant_suppliers' : self.id
+                         }
+                _logger.info('About to create %s', info)
+                sellers.append(info)
+
+
+        #sellers = [{'supplier': 1290, 'leadtime': 12}, {'supplier': 579, 'leadtime': 27}]
+        val['product_suppliers'] = [(0, 0, v) for v in sellers]
+        return val
+
+
+    def Xview_init(self, cr, uid, fields, context=None):
+        res = super(request_relevant_suppliers, self).view_init(cr, uid, fields, context)
+        _logger.info(' %s VIEW INIT CONTEXT %s', res, context)
+        active_id = context['active_id']
+        tender = self.pool('purchase.requisition').browse(cr, uid, [active_id])
+        products = [line.product_id for line in tender.line_ids]
+
+        for product in products:
+            for seller in product.seller_ids:
+                _logger.info('%s CREATING SELLER %s',fields, seller.name.name)
+
+                #supp = self.env['bc_website_purchase.relevant.supplierinfo'].create(
+                #    {'supplier' : seller.name.id,
+                #                                 'leadtime' : seller.delay,
+                #                                 'relevant_suppliers' : self.id
+                #    }
+                #)
+                info = {'name' : seller.name.id,
+                        'leadtime' : seller.delay,
+                        'relevant_suppliers' : self.id
+                         }
+                _logger.info('About to create %s', info)
+                self.product_suppliers = [(0,0, info)]
+                _logger.info('SUPP %s', self.product_suppliers)
+
+        if not tender.line_ids:
+            raise Warning(_('Error'), _('Define product(s) you want to include in the call for bids.'))
+
+        return res
+
+    @api.one
+    def create_order(self):
+        _logger.info('create_order in request_relevant_suppliers')
+        active_id = self.env.context['active_id']
+        _logger.info('create_order active_id %s' % active_id)
+        tender = self.env['purchase.requisition'].browse(active_id)
+
+        prods = self.env['relevant_supplierinfo'].search([('relevant_suppliers','=',self.id)])
+
+
+
+        _logger.info('create_order %s %s', self.id, prods)
+
+
+
+        for si in prods:
+            supplierinfo = si.read(['name', 'leadtime'])[0]
+
+            _logger.info('create_order %s %s', tender, supplierinfo['name'])
+            leadtime = supplierinfo['leadtime']
+            rfq_id = tender.make_purchase_order(supplierinfo['name'][0])
+            _logger.info('create_order rfq %s', rfq_id)
+            for rfq in rfq_id.values():
+                _logger.info('searching')
+                lines = self.env['purchase.order.line'].search([('order_id','=',rfq)])
+                _logger.info('Lines found %s', lines)
+                lines.write({'leadtime' : leadtime})
+
+        return {'type': 'ir.actions.act_window_close'}
+
     """
     def view_init(self, cr, uid, fields_list, context=None):
         if context is None:
@@ -29,3 +177,12 @@ class request_relevant_suppliers(models.TransientModel):
         self.pool.get('purchase.requisition').make_purchase_order(cr, uid, active_ids, data.partner_id.id, context=context)
         return {'type': 'ir.actions.act_window_close'}
     """
+
+class relevant_supplierinfo(models.TransientModel):
+    _name = 'relevant_supplierinfo'
+
+    # Basically a transient version of product.supplierinfo
+
+    relevant_suppliers = fields.Many2one('requisition_suppliers')
+    name = fields.Many2one('res.partner', 'Supplier')
+    leadtime = fields.Integer('Leadtime', help='Time from confirmed order to receipt of goods.')
